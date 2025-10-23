e2729ed4c2107862b843f94e5398d2e4f78a3777
Aluno <guilhermerosazini@gmail.com>
2025-10-23 20:57:58 -0300

    Correção do Trycatch

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require('express');
 const fs = require("fs");
 const app = express();
-const PORT = 8081;
+const PORT = 8080;
 
-try {
 
-    app.get("/usuarios", (req, res) => {
+app.get("/usuarios", (req, res) => {
+
+    try {
 
         const data = fs.readFileSync("./usuarios.json", "utf-8"); // LER ARQUIVO
         let usuarios = JSON.parse(data); // CONVERTER JSON PARA OBJETO JAVASCRIPT
+        const { nomeUsuario } = req.query;
 
-        const {nomeUsuario} = req.query;
-
-        if(nomeUsuario){
+        if (nomeUsuario) {
             usuarios = usuarios.filter(usuario => usuario.nome.toLowerCase()
-            .includes(nomeUsuario.toLowerCase()));
+                .includes(nomeUsuario.toLowerCase()));
         }
 
         res.status(200).json(usuarios);
 
-    })
-
-} catch (error) {
-    console.error("Erro ao ler o arquivo JSON", error);
-    res.status(500).json({ erro: "Erro interno no servidor ao processar os usuários!" })
-
-}
+    } catch (error) {
+        console.error("Erro ao ler o arquivo JSON", error);
+        res.status(500).json({ erro: "Erro interno no servidor ao processar os usuários!" })
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`O servidor está rodando em https://localhost:${PORT}`)
