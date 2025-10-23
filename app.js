a6365328b795e16c2d2bb6567db667564b7a8ce7
Aluno <guilhermerosazini@gmail.com>
2025-10-23 19:23:34 -0300

    EX 01

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require("fs");
+const app = express();
+const PORT = 8081;
+
+try {
+
+    app.get("/usuarios", (req, res) => {
+
+        const data = fs.readFileSync("./usuarios.json", "utf-8"); // LER ARQUIVO
+        const usuarios = JSON.parse(data); // CONVERTER JSON PARA OBJETO JAVASCRIPT
+
+        res.status(200).json(usuarios);
+
+    })
+
+} catch (error) {
+    console.error("Erro ao ler o arquivo JSON", error);
+    res.status(500).json({ erro: "Erro interno no servidor ao processar os usuários!" })
+
+}
+
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando em https://localhost:${PORT}`)
+})
